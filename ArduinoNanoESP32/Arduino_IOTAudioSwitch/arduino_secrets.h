641b215579b2497577f5a6d4dee15d1d71276641
Hiro Fujii <64285740+fujiihc@users.noreply.github.com>
2025-01-02 20:23:35 -0500

    Delete ArduinoNanoESP32/Arduino_IOTAudioSwitch directory

--- ArduinoNanoESP32/Arduino_IOTAudioSwitch/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_SSID "Your SSID/WiFi Network Here"
-#define SECRET_OPTIONAL_PASS "Your SSID/WiFi Password Here, Leave Blank if None"
-#define SECRET_DEVICE_KEY "Your Arduino Cloud Secret Key Here"
